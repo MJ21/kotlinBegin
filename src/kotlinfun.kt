68ef0d881d92527f36c2ca45109eac9a92cadfa2
mj21 <mflath21@yahoo.com>
2021-09-24 12:11:18 -0700

    Println tutuorial - the sum of two ints

--- src/kotlinfun.kt
+++ src/kotlinfun.kt
@@ -1,3 +1,8 @@
 fun main(args : Array<String>) {
+    // print the obligatory Hello world!
     println("Hello Kotlin world!")
+
+    // print the computation of two integers
+    val sum : Int = 1 + 2
+    println(sum)
 }
